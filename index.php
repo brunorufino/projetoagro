a208d2728721a4dbb38e16020b5fcfe6747e65ff
brunorufino <69933497+brunorufino@users.noreply.github.com>
2021-08-31 19:34:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Agro</title>
+</head>
+<body>
+<br>
+<br>
+<center>
+    <img src="img/logo.png" alt="logo">
+    <br>
+    <br>
+    <br>
+    <caption> </caption>
+    <div class="">
+        <img src="img/TELA1.png">
+        <img src="img/TELA2.png">
+        <img src="img/TELA3.png">
+  </div>
+</center>
+
+</body>
+</html>
